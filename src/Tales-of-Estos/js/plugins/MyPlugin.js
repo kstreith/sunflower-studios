902091bf89fecdd3976bebd475919e7b255ae26a
Oracle-exe <Oracle.ex3@gmail.com>
2023-07-13 21:41:31 -0400

    Created Tales of Estos

---
+++ src/Tales-of-Estos/js/plugins/MyPlugin.js
@@ -0,0 +1,22 @@
+//=============================================================================
+// PluginName.js
+//=============================================================================
+/*:
+ * @plugindesc Description of your plugin
+ *
+ * @author Your name
+ *
+ * @help
+ *
+ * Anything users might need to know about using your plugin.
+ *
+ * TERMS OF USE
+ * What people who use your plugin are allowed to do with it.
+ *
+ * COMPATIBILITY
+ * Any compatibility issues you know of or that have been brought to your attention, such as not being able to use this plugin with one of Galv's for example. This will be most common when you have a plugin that does the same or a similar thing to someone else's.
+ */
+ 
+(function() {
+    var parameters = PluginManager.parameters('MyPlugin');
+})();
